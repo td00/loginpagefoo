b5e1f139b3ef004f63a649bf9e6a14d9864b2eaa
Thies Mueller <git@td00.de>
2021-01-08 17:35:49 +0100

    that was a stupid mistake...

--- secure.php
+++ secure.php
@@ -26,7 +26,7 @@ echo "Your full name is: ".$usergn." ".$userln;
 echo "<br/>";
 echo "And your email is: ".$useremail;
 echo "<br/>";
-echo "You can logout <a href="logout.php">here</a>";
+echo 'You can logout <a href="logout.php">here</a>';
 echo "<br/>";
 echo "<br/>";
 echo "This is the end now!";
