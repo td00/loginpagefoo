4b8d3dc9ea6a29e6e12ce9514912f87b664f5124
Thies Mueller <git@td00.de>
2021-01-08 13:31:41 +0100

    print email address of user(?)

--- secure.php
+++ secure.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+$pdo = new PDO('mysql:host=localhost;dbname=usertable', 'usertable', 'password');
 if(!isset($_SESSION['userid'])) {
     die('Please <a href="login.php">login</a>');
 }
@@ -8,5 +9,16 @@ $userid = $_SESSION['userid'];
  
 echo "Hi ".$userid;
 echo "<br/>";
+echo "<br/>";
+echo "<br/>";
+$statement = $pdo->prepare("SELECT * FROM users WHERE id = $userid");
+        $result = $statement->execute(array('email' => $email));
+        $user = $statement->fetch();
+
+echo $email;
+echo "<br/>";
+echo "<br/>";
+echo "<br/>";
+
 echo "This is secure now!";
 ?>
