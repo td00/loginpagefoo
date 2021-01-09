422463e22c30de0903f91780ea71950ebd0a922d
Thies Mueller <git@td00.de>
2021-01-10 00:07:44 +0100

    just get to the page specified by "page" Get

--- update.php
+++ update.php
@@ -14,14 +14,7 @@ $_SESSION['lastName'] = $user['lastName'];
 $_SESSION['activated'] = $user['activated'];
 $_SESSION['updated_at'] = $user['updated_at'];
 
-if(isset($_GET['register'])) {
-echo 'Session newly validated. Going back to secure land! <meta http-equiv="refresh" content="0; URL=secure.php">';
-}if(isset($_GET['profile'])) {
-    echo 'Session newly validated. Going back to profile land! <meta http-equiv="refresh" content="0; URL=profile.php">';
-}if(isset($_GET['rawdata'])) {
-    echo 'Session newly validated. Going back to secure land! <meta http-equiv="refresh" content="0; URL=rawdata.php">';
-}else {
-    echo 'Session newly validated. I dont know where i shoudl go! Defaulting to secure! <meta http-equiv="refresh" content="0; URL=secure.php">';
-}
 
+echo 'Session newly validated!';
+echo '<meta http-equiv="refresh" content="0; URL='.$_GET['page'].'">';
 ?>
