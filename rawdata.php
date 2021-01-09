12cf218916a6a847844a2a37294f64ae0e0b5ca1
Thies Mueller <git@td00.de>
2021-01-09 23:44:04 +0100

    introducing rawdata.php for a test

---
+++ rawdata.php
@@ -0,0 +1,13 @@
+<?php
+echo $_SESSION['userid'];
+echo "<br />"
+echo $_SESSION['username'];
+echo "<br />"
+echo $_SESSION['email'];
+echo "<br />"
+echo $_SESSION['givenName'];
+echo "<br />"
+echo $_SESSION['lastName'];
+echo "<br />"
+echo $_SESSION['activated'];
+?>
