67bd5bc1c7c2113da58817ff41f15fffda2e58e4
thies <git@td00.de>
2021-01-10 19:00:04 +0100

    introducing backgroundupdate

---
+++ backgroundupdate.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+
+include 'db.inc.php';
+$username = $_SESSION['username'];
+
+$statement = $pdo->prepare("SELECT * FROM users WHERE username = :username");
+$result = $statement->execute(array('username' => $username));
+$user = $statement->fetch();
+$_SESSION['userid'] = $user['id'];
+$_SESSION['email'] = $user['email'];
+$_SESSION['username'] = $user['username'];
+$_SESSION['givenName'] = $user['givenName'];
+$_SESSION['lastName'] = $user['lastName'];
+$_SESSION['activated'] = $user['activated'];
+$_SESSION['updated_at'] = $user['updated_at'];
+$_SESSION['isadmin'] = $user['isadmin'];
+?>
