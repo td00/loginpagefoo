67d151f98b50ebc89d6141f12e6f62ab0ccc8ea4
Thies Mueller <git@td00.de>
2021-01-10 17:55:05 +0100

    removed prod db

--- db.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqlhost = 'localhost';
-$dbname = 'usertable';
-$dbuser = 'usertable';
-$dbpass = 'password';
-$pdo = new PDO('mysql:host='.$mysqlhost.';dbname='.$dbname.'', $dbuser , $dbpass);
-//$pdo = new PDO('mysql:host=localhost;dbname=usertable', 'usertable', 'password');
-?>
-
