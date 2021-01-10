eacbf81a9aead2dd8b0b161528ccf70c9f4310ae
Thies Mueller <git@td00.de>
2021-01-10 12:07:36 +0100

    introducing my db connection

---
+++ db.inc.php
@@ -0,0 +1,10 @@
+<?php
+$mysqlhost = 'localhost';
+$dbname = 'usertable';
+$dbuser = 'usertable';
+$dbpass = 'password';
+$pdo = new PDO('mysql:host='.$mysqlhost.';dbname='.$dbname.'', $dbuser , $dbpass);
+
+?>
+
+<h1>NOPE</h1>
