8d2ffcde8a37dbaa63eeb2a5806483d79ec6ca0b
Thies Mueller <git@td00.de>
2021-01-12 17:38:49 +0100

    commented activatedarea.php

--- activatedarea.php
+++ activatedarea.php
@@ -7,20 +7,21 @@
 <body>
 <script src="ressources/js/bootstrap.min.js"></script>
 <?php
-session_start();
-if($_SESSION['activated'] == 0) {
+session_start(); //check for a session
+if($_SESSION['activated'] == 0) { //check if the account is activated, if not, die with an error
     die ("Not activated yet");
 }
-echo "heres the fun world";
+echo "heres the fun world"; //just a placeholder
 ?>
 <br /> <br />
 <div class="jumbotron jumbotron-fluid">
   <div class="container">
 <?php
 echo "Hi ".$_SESSION['username']."!";
-if(isset($_GET['notimplemented'])) {
+if(isset($_GET['notimplemented'])) { //if "?notimplemented=1" is received, print the following error code:
     echo '<div class="alert alert-danger" role="alert">Feature not yet implemented!</div>';
 }
+//some html links to other pages
 ?>
 <br /><br />
 <a href="changeprofilepicture.php"><button class="btn btn-primary">Change Profile Picture</button>
